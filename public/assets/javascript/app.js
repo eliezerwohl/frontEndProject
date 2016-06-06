89bd93d1bc8c57cf3bb2995e0122c8f38d769b6f
eliezerwohl <eliezerwohl@gmail.com>
2016-06-06 11:35:31 -0400

    added ui router

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,1 +1,16 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider){
+	$urlRouterProvider.otherwise('/index')
+ $stateProvider
+  // HOME STATES AND NESTED VIEWS ========================================
+    .state('index', {
+      url: '/index',
+      templateUrl: '../views/home.html'
+    })
+     .state('about', {
+      url: '/about',
+      templateUrl: '../views/about.html'
+    })
+
+    })
