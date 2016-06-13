396ea4687fdc90599eef15efe367ec9a727c43ae
eliezerwohl <eliezerwohl@gmail.com>
2016-06-13 09:22:25 -0400

    changed the style of the demo buttons

--- public/assets/javascript/mainpage.js
+++ public/assets/javascript/mainpage.js
@@ -1,105 +1,49 @@
-// $("navLogo").hide();
 var logo = ["L", "i", "b", "e", "r", "t", "y", " ", "V", "i", "c", "e"]
 setTimeout(fancyFade, 1000);
 navigation()
 
-// $("#guitar").on("mouseover", function(){
-// 	$("#guitar").flip(); 
-// })
-// $(function($) {
-//   $(".card-grid").flip(); 
-// });
-
-$("#guitar").flip({
-  axis: 'x',
-  trigger: 'hover'
-});
-$("#bass").flip({
-  axis: 'x',
-  trigger: 'hover'
-});
-$("#keys").flip({
-  axis: 'x',
-  trigger: 'hover'
-});
-$("#drums").flip({
-  axis: 'x',
-  trigger: 'hover'
-});
-
-function navigation(){
-
-$("li").on("click", function(){
-	$('li').off()
-	$(".section").fadeOut(200)
-	var id = $(this).data("id")
-	navbar(id)
+function navigation() {
+	$("li").on("click", function() {
+		$('li').off()
+		$(".section").fadeOut(200)
+		var id = $(this).data("id")
+		navbar(id)
 	});
 }
 
-
-
-function navbar(id){
+function navbar(id) {
 	setTimeout(navAnimation, 575)
-	function navAnimation(){
+
+	function navAnimation() {
 		$("#" + id).fadeIn()
 		navigation()
-	}		
-}
-
-// $("#demos").on("click", function(){
-// 	setTimeout(demoGo, 500)
-// 	function demoGo(){
-// 	$("#demosSection").fadeIn()
-// 	}
-// })
-// $("#bandHistory").on("click", function(){
-// 	setTimeout(demoGo, 500)
-// 	function demoGo(){
-// 	$("#bandHistorySection").fadeIn()
-// 	}
-// })
-// $("#members").on("click", function(){
-// 	setTimeout(demoGo, 500)
-// 	function demoGo(){
-// 	$("#membersSection").fadeIn()
-// 	}
-// })
-// $("#contactUs").on("click", function(){
-// 	setTimeout(demoGo, 500)
-// 	function demoGo(){
-// 	$("#contactUsSection").fadeIn()
-// 	}
-// })
-
-function fancyFade(){
-
-var i = 0
-function print(){
-	 $("#logo").append(logo[i]);
-	 i++
-	 if (i === 12){
-	 clearInterval(printLogo);
-		var fadeLogo = setInterval(logoFunction,40)
-		var opacity = 0
-		function logoFunction(){
-		opacity += .02
-			if (opacity > 1){
-			console.log("done")
-			clearInterval(fadeLogo)
-		}
-		console.log(opacity)
-	// console.log(opacity )
-	$(".navbar").css('opacity', opacity)
-}
-
 	}
 }
-var printLogo = setInterval(print,75);
+
+function fancyFade() {
+	var i = 0
+	function print() {
+		$("#logo").append(logo[i]);
+		i++
+		if (i === 12) {
+			clearInterval(printLogo);
+			var fadeLogo = setInterval(logoFunction, 40)
+			var opacity = 0
+
+			function logoFunction() {
+				opacity += .02
+				if (opacity > 1) {
+					console.log("done")
+					clearInterval(fadeLogo)
+				}
+				console.log(opacity)
+					// console.log(opacity )
+				$(".navbar").css('opacity', opacity)
+			}
+
+		}
+	}
+	var printLogo = setInterval(print, 75);
 }
-
-
-		// $("a").removeClass("selected")
-	// $(this).addClass("selected")
 
 
